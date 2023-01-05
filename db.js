c4808f3fca0763b0dfef4a6225e3f660ca8e15b7
LuisBaezN <baez.201041081@gmail.com>
2023-01-04 20:36:08 -0600

    add: line deleted

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config(); //process.env.URI at MongoClient.connect()
 
 let dbConnection;
-//uri = 'mongodb+srv://Luis:7euVEUmx1ihZHKm5@books.uqc0saf.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
     connectToDb: (cb) => {
